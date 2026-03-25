52560277682e9d9daf93910d0ef742cae2168dd3
architha <architha.e@ninestar.co.in>
2026-03-25 19:38:04 +0530

    url modified

--- PgFinder/authentication/urls.py
+++ PgFinder/authentication/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 urlpatterns=[
     path('login',views.user_login,name='login'),
+    path('',views.user_login,name='login'),
+
     path('signin',views.signin,name='signin'),
     path('logout',views.user_logout,name='logout'),
 #     path('',views.home,name='home')
